d418120f6a92c0adf3eb0a83f95f4b65d061d2fb
saddamarbaa <saddamarbaas@gamil.com>
2022-12-28 11:00:42 +0700

    feat(api): add pdf file serve api

---
+++ src/routes/pdfDoc.route.ts
@@ -0,0 +1,31 @@
+import express, { NextFunction, Request, Response } from 'express';
+import path from 'path';
+import fs, { createReadStream } from 'fs';
+import createHttpError from 'http-errors';
+
+const router = express.Router();
+
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const pdfName = `sample.pdf`;
+    const folderFullPath = path.resolve(process.cwd(), `${process.env.PWD}/public/pdf/${pdfName}`);
+
+    fs.stat(folderFullPath, async function (err, stats) {
+      console.log(stats); // here we got all information of file in stats variable
+      if (err) {
+        console.log('fail to find file path', err);
+        return next(createHttpError(`Fail to find file path`));
+      }
+
+      // const result = await fsPromises.readFile(folderFullPath);
+      const stream = await createReadStream(folderFullPath);
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', `attachment; filename=${pdfName}`);
+      stream.pipe(res);
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+export = router;
