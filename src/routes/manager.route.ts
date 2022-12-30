be250ded1c3473954f9a2597e2071f0d23ac78be
saddamarbaa <saddamarbaas@gamil.com>
2022-12-30 19:26:53 +0700

    feat(api): add  manager get orders list api

--- src/routes/manager.route.ts
+++ src/routes/manager.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { environmentConfig } from '@src/configs';
-import { managerGetUsersController } from '@src/controllers';
+import { managerGetOrdersController, managerGetUsersController } from '@src/controllers';
 import { customRoles, isAuth } from '@src/middlewares';
 
 const router = express.Router();
 
 router.get('/users', isAuth, customRoles(environmentConfig.MANGER_EMAILS), managerGetUsersController);
+router.get('/orders', isAuth, customRoles(environmentConfig.MANGER_EMAILS), managerGetOrdersController);
 
 export = router;
