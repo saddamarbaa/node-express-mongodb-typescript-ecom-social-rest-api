2217b582b57a59b6dda2d6be6e0318c2998c32f7
saddamarbaa <saddamarbaas@gamil.com>
2023-01-01 19:23:09 +0700

    feat(api): add api that return stripe public key

---
+++ src/services/payment.service.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Response } from 'express';
+import createHttpError from 'http-errors';
+
+import Stripe from 'stripe';
+import { customResponse } from '@src/utils';
+import { environmentConfig } from '@src/configs';
+import { AuthenticatedRequestBody, IUser, ProcessingStripeCheckoutT } from '@src/interfaces';
+
+const stripe = new Stripe(environmentConfig.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2022-11-15',
+  appInfo: {
+    // For sample support and debugging, not required for production:
+    name: 'stripe-samples/accept-a-payment',
+    url: 'https://github.com/stripe-samples',
+    version: '0.0.2',
+  },
+  typescript: true,
+});
+
+export const getStripePublicKeyService = async (
+  req: AuthenticatedRequestBody<IUser>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const data = {
+      stripeKey: environmentConfig.STRIPE_PUBLIC_KEY,
+    };
+
+    return res.status(200).send(
+      customResponse<typeof data>({
+        success: true,
+        error: false,
+        message: `Success`,
+        status: 200,
+        data,
+      })
+    );
+  } catch (error) {
+    return next(createHttpError.InternalServerError);
+  }
+};
