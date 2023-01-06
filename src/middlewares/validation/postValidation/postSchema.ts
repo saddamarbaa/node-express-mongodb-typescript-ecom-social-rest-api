a264c87a6ef885c10c217656094b02fcb66d223e
saddamarbaa <saddamarbaas@gamil.com>
2023-01-06 23:11:12 +0700

    chore add validation to create post api schema

---
+++ src/middlewares/validation/postValidation/postSchema.ts
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+
+import { postCategory } from '@src/constants';
+
+export const postSchema = {
+  addPost: Joi.object({
+    title: Joi.string().min(3).max(100).required(),
+    content: Joi.string().min(5).required(),
+    category: Joi.string()
+      .required()
+      .valid(
+        postCategory.blockchain,
+        postCategory.coding,
+        postCategory.devApp,
+        postCategory.nextjs,
+        postCategory.nodejs,
+        postCategory.reactjs,
+        postCategory.sports,
+        postCategory.typeScript,
+        postCategory.social
+      ),
+    filename: Joi.string().required().label('Invalid request (Please upload Image)'),
+  }),
+};
