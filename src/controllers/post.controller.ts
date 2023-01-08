df123208fdd183c4de574c1970679d5757b10422
saddamarbaa <saddamarbaas@gamil.com>
2023-01-08 21:40:37 +0700

    feat(api): add user get single post api

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,8 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
-import { AuthenticatedRequestBody, PostT, TPaginationResponse } from '@src/interfaces';
-import { createPostService, getPostsService } from '@src/services';
+import { AuthenticatedRequestBody, IUser, PostT, TPaginationResponse } from '@src/interfaces';
+import { createPostService, getPostService, getPostsService } from '@src/services';
 
 export const getPostsController = (req: Request, res: TPaginationResponse) => getPostsService(req, res);
+
+export const getPostController = (req: AuthenticatedRequestBody<IUser>, res: Response, next: NextFunction) =>
+  getPostService(req, res, next);
 
 export const createPostController = (req: AuthenticatedRequestBody<PostT>, res: Response, next: NextFunction) =>
   createPostService(req, res, next);
--- src/routes/feed.route.ts
+++ src/routes/feed.route.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
-import { createPostController, getPostsController } from '@src/controllers';
-import { addPostValidation, isAuth, postPaginationMiddleware, uploadImage } from '@src/middlewares';
+import { createPostController, getPostController, getPostsController } from '@src/controllers';
+import { addPostValidation, isAuth, postIdValidation, postPaginationMiddleware, uploadImage } from '@src/middlewares';
 
 const router = express.Router();
 
 router.get('/posts', postPaginationMiddleware(), getPostsController);
-
+router.get('/posts/:postId', postIdValidation, getPostController);
 router.post('/posts', uploadImage.single('postImage'), isAuth, addPostValidation, createPostController);
 export = router;
