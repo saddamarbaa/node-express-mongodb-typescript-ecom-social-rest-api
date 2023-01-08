d5c14a159b04f6e9b012d7b74401da7f34cceffd
saddamarbaa <saddamarbaas@gamil.com>
2023-01-08 18:35:08 +0700

    fix: remove required from category filed in add post schema validation

--- src/middlewares/validation/postValidation/postSchema.ts
+++ src/middlewares/validation/postValidation/postSchema.ts
@@ -6,19 +6,17 @@ export const postSchema = {
   addPost: Joi.object({
     title: Joi.string().min(3).max(100).required(),
     content: Joi.string().min(5).required(),
-    category: Joi.string()
-      .required()
-      .valid(
-        postCategory.blockchain,
-        postCategory.coding,
-        postCategory.devApp,
-        postCategory.nextjs,
-        postCategory.nodejs,
-        postCategory.reactjs,
-        postCategory.sports,
-        postCategory.typeScript,
-        postCategory.social
-      ),
+    category: Joi.string().valid(
+      postCategory.blockchain,
+      postCategory.coding,
+      postCategory.devApp,
+      postCategory.nextjs,
+      postCategory.nodejs,
+      postCategory.reactjs,
+      postCategory.sports,
+      postCategory.typeScript,
+      postCategory.social
+    ),
     filename: Joi.string().required().label('Invalid request (Please upload Image)'),
   }),
 };
