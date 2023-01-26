8ce9d90e3b7945d54fe8a1eb25dac8d1eee1a524
saddamarbaa <saddamarbaas@gamil.com>
2023-01-26 19:44:33 +0700

    test(api): test admin add product api

--- __tests__/integration/product.test.ts
+++ __tests__/integration/product.test.ts
@@ -47,67 +47,6 @@ afterEach(async () => {
 
 describe('product', () => {
   /**
-   * Testing get all products endpoint
-   */
-  describe('GET /api/v1/products', () => {
-    describe('given no product in db', () => {
-      it('should return empty array with a 200', async () => {
-        request(app)
-          .get('/api/v1/products')
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            return expect(response.body.data).toMatchObject({
-              totalDocs: 0,
-              totalPages: 0,
-              lastPage: 0,
-              count: 0,
-              currentPage: { page: 1, limit: 20 },
-              products: [],
-            });
-          });
-      });
-    });
-
-    describe('given added 2 product in db', () => {
-      it('should return array of 2 product with a 200', async () => {
-        cloudinary.v2.uploader.upload = jest.fn().mockResolvedValue(cloudinaryResponse);
-
-        // create user
-        const newUser = new User({
-          ...userPayload,
-        });
-
-        await newUser.save();
-
-        const newProduct = { ...productPayload, user: newUser?._id };
-        await Product.insertMany([
-          {
-            ...newProduct,
-          },
-          {
-            ...newProduct,
-          },
-        ]);
-
-        request(app)
-          .get('/api/v1/products')
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            expect(response.body.data).toMatchObject({
-              totalDocs: 2,
-            });
-            expect(response?.body?.data?.products?.length).toBe(2);
-            expect(response?.body?.data?.products[0].name).toMatch(productPayload.name);
-          });
-      });
-    });
-  });
-
-  /**
    * Testing get single product endpoint
    */
   describe('GET /api/v1/products/:productId', () => {
@@ -178,6 +117,265 @@ describe('product', () => {
           .expect(200);
 
         expect(finalResult?.body?.data?.product).toMatchObject(productPayload);
+      });
+    });
+  });
+
+  /**
+   * Testing get all products endpoint
+   */
+  describe('GET /api/v1/products', () => {
+    describe('given no product in db', () => {
+      it('should return empty array with a 200', async () => {
+        request(app)
+          .get('/api/v1/products')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .then((response) => {
+            return expect(response.body.data).toMatchObject({
+              totalDocs: 0,
+              totalPages: 0,
+              lastPage: 0,
+              count: 0,
+              currentPage: { page: 1, limit: 20 },
+              products: [],
+            });
+          });
+      });
+    });
+
+    describe('given added 2 product in db', () => {
+      it('should return array of 2 product with a 200', async () => {
+        cloudinary.v2.uploader.upload = jest.fn().mockResolvedValue(cloudinaryResponse);
+
+        // create user
+        const newUser = new User({
+          ...userPayload,
+        });
+
+        await newUser.save();
+
+        const newProduct = { ...productPayload, user: newUser?._id };
+        await Product.insertMany([
+          {
+            ...newProduct,
+          },
+          {
+            ...newProduct,
+          },
+        ]);
+
+        request(app)
+          .get('/api/v1/products')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .then((response) => {
+            expect(response.body.data).toMatchObject({
+              totalDocs: 2,
+            });
+            expect(response?.body?.data?.products?.length).toBe(2);
+            expect(response?.body?.data?.products[0].name).toMatch(productPayload.name);
+          });
+      });
+    });
+  });
+
+  /**
+   * Testing post product endpoint
+   */
+  describe('POST /api/v1/admin/products/add', () => {
+    describe('given the user is not logged in', () => {
+      it('should return a 401 status with a json message - Auth Failed', async () => {
+        request(app)
+          .post('/api/v1/admin/products/add')
+          .attach('productImages', localFilePath)
+          .expect(401)
+          .then((response) =>
+            expect(response.body).toMatchObject({
+              data: null,
+              success: false,
+              error: true,
+              message: expect.any(String),
+              status: 401,
+              stack: expect.any(String),
+            })
+          )
+          .catch((error) => {
+            console.log(error);
+          });
+      });
+    });
+
+    describe('given the user is logged in and the logged user is not admin', () => {
+      it('should return a 403 status with a json message - unauthorized', async () => {
+        // create user
+        const newUser = new User({
+          ...userPayload,
+        });
+        await newUser.save();
+
+        // Login to get token and user id
+        const authResponse = await request(app)
+          .post('/api/v1/auth/login')
+          .send({ email: userPayload.email, password: userPayload.password });
+        const TOKEN = authResponse?.body?.data?.accessToken || '';
+
+        request(app)
+          .post('/api/v1/admin/products/add')
+          .set('Authorization', `Bearer ${TOKEN}`)
+          .attach('productImages', localFilePath)
+          .expect(403)
+          .then((response) => {
+            expect(response.body).toMatchObject({
+              data: null,
+              success: false,
+              error: true,
+              message: expect.any(String),
+              status: 403,
+              stack: expect.any(String),
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      });
+    });
+
+    describe('given the user is logged in and authorized', () => {
+      describe('given invaild product', () => {
+        it('should validated product schema and return a 422 status with validation message', async () => {
+          // create user
+          const newUser = new User({
+            ...userPayload,
+            email: (adminEmails && adminEmails[0]) || userPayload.email,
+            role: authorizationRoles.admin,
+          });
+          await newUser.save();
+
+          // Login to get token and user id
+          const authResponse = await request(app)
+            .post('/api/v1/auth/login')
+            .send({
+              email: (adminEmails && adminEmails[0]) || userPayload.email,
+              password: userPayload.password,
+            });
+          const TOKEN = authResponse?.body?.data?.accessToken || '';
+
+          request(app)
+            .post('/api/v1/admin/products/add')
+            .set('Authorization', `Bearer ${TOKEN}`)
+            .attach('productImages', localFilePath)
+            .expect('Content-Type', /json/)
+            .expect(422)
+            .then((response) => {
+              expect(response.body).toMatchObject({
+                data: null,
+                success: false,
+                error: true,
+                message: expect.any(String),
+                status: 422,
+                stack: expect.any(String),
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+      });
+
+      describe('given invaild product with no images been attached', () => {
+        it('should validated file upload and return a 422 status with validation message', async () => {
+          // create user
+          const newUser = new User({
+            ...userPayload,
+            email: (adminEmails && adminEmails[0]) || userPayload.email,
+            role: authorizationRoles.admin,
+          });
+          await newUser.save();
+
+          // Login to get token and user id
+          const authResponse = await request(app)
+            .post('/api/v1/auth/login')
+            .send({
+              email: (adminEmails && adminEmails[0]) || userPayload.email,
+              password: userPayload.password,
+            });
+          const TOKEN = authResponse?.body?.data?.accessToken || '';
+
+          request(app)
+            .post('/api/v1/admin/products/add')
+            .set('Authorization', `Bearer ${TOKEN}`)
+            // .attach('productImages', localFilePath)
+            .expect('Content-Type', /json/)
+            .expect(422)
+            .then((response) => {
+              expect(response.body).toMatchObject({
+                data: null,
+                success: false,
+                error: true,
+                message: expect.any(String),
+                status: 422,
+                stack: expect.any(String),
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+      });
+
+      describe('given vaild product ', () => {
+        it('should return a 201 status and create the product', async () => {
+          // create user
+          const newUser = new User({
+            ...userPayload,
+            email: (adminEmails && adminEmails[0]) || userPayload.email,
+            role: authorizationRoles.admin,
+          });
+          await newUser.save();
+
+          // Login to get token and user id
+          const authResponse = await request(app)
+            .post('/api/v1/auth/login')
+            .send({
+              email: (adminEmails && adminEmails[0]) || userPayload.email,
+              password: userPayload.password,
+            });
+          const TOKEN = authResponse?.body?.data?.accessToken || '';
+
+          request(app)
+            .post('/api/v1/admin/products/add')
+            .field({
+              name: productPayload.name,
+              price: productPayload.price,
+              brand: productPayload.price,
+              description: productPayload.description,
+            })
+            .set('Authorization', `Bearer ${TOKEN}`)
+            .set('Content-Type', 'multipart/form-data')
+            .attach('productImages', localFilePath)
+            .expect('Content-Type', /json/)
+            .expect(201)
+            .then((response) => {
+              expect(response.body).toMatchObject({
+                success: true,
+                error: false,
+                message: expect.any(String),
+                status: 201,
+              });
+
+              expect(response.body?.data?.product).toMatchObject({
+                name: productPayload.name,
+                price: productPayload.price,
+                description: productPayload.description,
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
       });
     });
   });
