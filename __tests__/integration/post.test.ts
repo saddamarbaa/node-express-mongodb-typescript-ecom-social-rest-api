42c8f8b4d508e325dfa309b70b19bbb4fea07b2a
saddamarbaa <saddamarbaas@gamil.com>
2023-02-04 22:02:09 +0700

    test(api): test get one comment api

--- __tests__/integration/post.test.ts
+++ __tests__/integration/post.test.ts
@@ -825,4 +825,227 @@ describe('Post', () => {
       });
     });
   });
+
+  /**
+   * Testing get single comment endpoint
+   */
+  describe('GET  /api/v1/feed/posts/comment/{postId}/{commentId}', () => {
+    describe('given the user is not logged in', () => {
+      it('should return a 401 status with a json message - Auth Failed', async () => {
+        request(app)
+          .get('/api/v1/feed/posts/comment/postId/commentId')
+          .expect(401)
+          .then((response) =>
+            expect(response.body).toMatchObject({
+              data: null,
+              success: false,
+              error: true,
+              message: expect.any(String),
+              status: 401,
+              stack: expect.any(String),
+            })
+          );
+      });
+    });
+
+    describe('given post id or comment id is not valid ', () => {
+      it('should return a 422 status with validation message', async () => {
+        const newUser = new User({
+          ...userPayload,
+          email: (adminEmails && adminEmails[0]) || userPayload.email,
+          role: authorizationRoles.admin,
+        });
+        await newUser.save();
+
+        const authResponse = await request(app)
+          .post('/api/v1/auth/login')
+          .send({
+            email: (adminEmails && adminEmails[0]) || userPayload.email,
+            password: userPayload.password,
+          });
+
+        const token = (authResponse && authResponse?.body?.data?.accessToken) || '';
+
+        if (token) {
+          // postId not vaild
+          await request(app)
+            .get(`/api/v1/feed/posts/comment/notvaild/${validMongooseObjectId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .then((response) => {
+              expect(response.body).toMatchObject({
+                data: null,
+                error: true,
+                status: 422,
+                message: expect.any(String),
+                stack: expect.any(String),
+              });
+              expect(response?.body?.message).toMatch(/fails to match the valid mongo id pattern/);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+
+          // commentId not vaild
+          await request(app)
+            .get(`/api/v1/feed/posts/comment/notvaild/${validMongooseObjectId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .then((response) => {
+              expect(response.body).toMatchObject({
+                data: null,
+                error: true,
+                status: 422,
+                message: expect.any(String),
+                stack: expect.any(String),
+              });
+              expect(response?.body?.message).toMatch(/fails to match the valid mongo id pattern/);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      });
+    });
+
+    describe('given the post does not exist', () => {
+      it('should return a 400 status with a json message - bad request', async () => {
+        const newUser = new User({
+          ...userPayload,
+          email: (adminEmails && adminEmails[0]) || userPayload.email,
+          role: authorizationRoles.admin,
+        });
+        await newUser.save();
+
+        const authResponse = await request(app)
+          .post('/api/v1/auth/login')
+          .send({
+            email: (adminEmails && adminEmails[0]) || userPayload.email,
+            password: userPayload.password,
+          });
+
+        const token = (authResponse && authResponse?.body?.data?.accessToken) || '';
+
+        if (token) {
+          await request(app)
+            .get(`/api/v1/feed/posts/comment/${validMongooseObjectId}/${validMongooseObjectId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .then((response) => {
+              expect(response.body).toMatchObject({
+                data: null,
+                success: false,
+                error: true,
+                message: expect.any(String),
+                status: 400,
+                stack: expect.any(String),
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      });
+    });
+
+    describe('given the user is logged in and authorized and the given post does exist in DB but not the comment does not exist', () => {
+      it('should return a 400 status with a json message', async () => {
+        const user = new User({
+          ...userPayload,
+          email: (adminEmails && adminEmails[0]) || userPayload.email,
+          role: authorizationRoles.admin,
+        });
+
+        await user.save();
+
+        const post = new Post({ ...postPayload, author: user._id });
+        await post.save();
+
+        const authResponse = await request(app)
+          .post('/api/v1/auth/login')
+          .send({
+            email: (adminEmails && adminEmails[0]) || userPayload.email,
+            password: userPayload.password,
+          });
+
+        const token = (authResponse && authResponse?.body?.data?.accessToken) || '';
+
+        if (token) {
+          await request(app)
+            .get(`/api/v1/feed/posts/comment/${post?._id}/${validMongooseObjectId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .expect('Content-Type', /json/)
+            .then((response) => {
+              expect(response.body).toMatchObject({
+                data: null,
+                success: false,
+                error: true,
+                message: expect.any(String),
+                status: 400,
+                stack: expect.any(String),
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      });
+    });
+
+    describe('given the user is logged in and authorized and the post and comment does exist', () => {
+      it('should return a 200 status with the post with only one comment', async () => {
+        const user = new User({
+          ...userPayload,
+          email: (adminEmails && adminEmails[0]) || userPayload.email,
+          role: authorizationRoles.admin,
+        });
+
+        await user.save();
+
+        const testComment = 'testComment';
+        const post = new Post({
+          ...postPayload,
+          author: user._id,
+          comments: [
+            {
+              user: user?._id,
+              comment: 'test',
+            },
+            {
+              user: user?._id,
+              comment: testComment,
+            },
+          ],
+        });
+        await post.save();
+
+        const authResponse = await request(app)
+          .post('/api/v1/auth/login')
+          .send({
+            email: (adminEmails && adminEmails[0]) || userPayload.email,
+            password: userPayload.password,
+          });
+
+        const token = (authResponse && authResponse?.body?.data?.accessToken) || '';
+
+        if (post && token) {
+          await request(app)
+            .get(`/api/v1/feed/posts/comment/${post?._id}/${post?.comments[1]?._id}`)
+            .set('Authorization', `Bearer ${token}`)
+            .expect('Content-Type', /json/)
+            .then((response) => {
+              expect(response.body).toMatchObject({
+                success: true,
+                error: false,
+                message: expect.any(String),
+                status: 200,
+              });
+              expect(response?.body?.data?.post?.comments?.length).toBe(1);
+              expect(response?.body?.message).toMatch('Successfully found comment');
+              expect(response?.body?.data?.post?.comments[0]?.comment).toMatch(testComment);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      });
+    });
+  });
 });
